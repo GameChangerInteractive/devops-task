82612cbe589a5655ae86be904c84dab8d058bde8
drscat <ikkv@yandex.ru>
2021-06-19 11:37:23 +0300

    fix redis-cache

--- src/infra/variables.tf
+++ src/infra/variables.tf
@@ -1,9 +1,7 @@
 variable "prefix" {
   description = "The Prefix used for all resources in this example"
-#   default = ""
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-#   default = ""
 }
