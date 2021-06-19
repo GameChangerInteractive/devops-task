ca56dccd203b3f4c4183e14a38dda6d7ffa73e4c
drscat <ikkv@yandex.ru>
2021-06-19 11:04:51 +0300

    add terraform

---
+++ src/infra/main.tf
@@ -0,0 +1,14 @@
+# Versions - https://releases.hashicorp.com/terraform-provider-azurerm/
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
