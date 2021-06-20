35979bc38c0a2815f483af810263d2a8f29fe018
drscat <ikkv@yandex.ru>
2021-06-20 13:57:19 +0300

    for terraform apply

--- src/infra/redis-cache.tf
+++ src/infra/redis-cache.tf
@@ -3,7 +3,6 @@ resource "azurerm_resource_group" "redisxmpl" {
   location = "westus"
 }
 
-# NOTE: the Name used for Redis needs to be globally unique
 resource "azurerm_redis_cache" "redisxmpl" {
   name                = "redisxmpl-cache"
   location            = azurerm_resource_group.redisxmpl.location
@@ -17,19 +16,3 @@ resource "azurerm_redis_cache" "redisxmpl" {
   redis_configuration {
   }
 }
-
-# resource "azurerm_resource_group" "redisxmpl" {
-#   name     = "${var.prefix}-resources"
-#   location = "${var.location}"
-# }
-
-# # NOTE: the Name used for Redis needs to be globally unique
-# resource "azurerm_redis_cache" "redisxmpl" {
-#   name                = "${var.prefix}-redis"
-#   location            = "${azurerm_resource_group.redisxmpl.location}"
-#   resource_group_name = "${azurerm_resource_group.redisxmpl.name}"
-#   capacity            = 0
-#   family              = "C"
-#   sku_name            = "Basic"
-#   enable_non_ssl_port = false
-# }
